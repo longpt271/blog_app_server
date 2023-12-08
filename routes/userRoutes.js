2d54bc8ff4a7e246fe44e489ddabce229d317935
longpt271 <longpt271@gmail.com>
2023-12-08 23:38:36 +0700

    user updateProfile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,11 +4,13 @@ import {
   registerUser,
   loginUser,
   userProfile,
+  updateProfile,
 } from "../controllers/userControllers";
 import { authGuard } from "../middleware/authMiddleware";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authGuard, userProfile);
+router.put("/updateProfile", authGuard, updateProfile);
 
 export default router;
