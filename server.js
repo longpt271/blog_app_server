8788a5b3f8a0a0849bd6bd03407a3387cf3897e0
longpt271 <longpt271@gmail.com>
2023-12-08 17:44:17 +0700

    setup port

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 
-import { my_name } from "./data";
+dotenv.config();
+const app = express();
 
-console.log(my_name);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
