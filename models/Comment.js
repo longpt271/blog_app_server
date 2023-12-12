3b62b08e6194608f1cac2cb2ebe3d3edac575ef9
longpt271 <longpt271@gmail.com>
2023-12-12 21:07:39 +0700

    START with post: creating Post, Comment and Categories model

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const CommentSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    desc: { type: String, required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    check: { type: Boolean, default: false },
+    parent: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    },
+    replyOnUser: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = model("Comment", CommentSchema);
+export default Comment;
