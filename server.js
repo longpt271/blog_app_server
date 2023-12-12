16d4077a233636d31be1d7ceb03798aec5ab0c7c
longpt271 <longpt271@gmail.com>
2023-12-12 19:05:16 +0700

    static assets upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db";
 import cors from "cors";
 import {
@@ -21,6 +22,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+
+// static assets
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(invalidPathHandler);
 app.use(errorResponserHandler);
