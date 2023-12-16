a3ae31baba774e0d1f0a35a8e4a5d385cc53eae2
longpt271 <longpt271@gmail.com>
2023-12-16 23:05:16 +0700

    change name PostCategories Schema

--- models/PostCategories.js
+++ models/PostCategories.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const PostCategoriesSchema = new Schema(
   {
-    title: { type: String, required: true },
+    name: { type: String, required: true },
   },
   { timestamps: true }
 );
