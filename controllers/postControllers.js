e83fc43927523b3a3fe0fb894058074451db5360
longpt271 <longpt271@gmail.com>
2023-12-18 16:19:24 +0700

    Dynamic Search & Pagination of the posts

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -145,19 +145,43 @@ const getPost = async (req, res, next) => {
 
 const getAllPosts = async (req, res, next) => {
   try {
-    const post = await Post.find({}).populate([
-      {
-        path: "user",
-        select: ["avatar", "name", "verified"],
-      },
-    ]);
+    const filter = req.query.searchKeyword;
+    let where = {};
+    if (filter) {
+      where.title = { $regex: filter, $options: "i" };
+    }
+    let query = Post.find(where);
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * pageSize;
+    const total = await Post.find(where).countDocuments();
+    const pages = Math.ceil(total / pageSize);
 
-    if (!post) {
-      const error = new Error("Post was not found");
+    if (page > pages) {
+      const error = new Error("No page found");
       return next(error);
     }
 
-    return res.json(post);
+    const result = await query
+      .skip(skip)
+      .limit(pageSize)
+      .populate([
+        {
+          path: "user",
+          select: ["avatar", "name", "verified"],
+        },
+      ])
+      .sort({ updatedAt: "desc" });
+
+    res.header({
+      "x-filter": filter,
+      "x-totalcount": JSON.stringify(total),
+      "x-currentpage": JSON.stringify(page),
+      "x-pagesize": JSON.stringify(pageSize),
+      "x-totalpagecount": JSON.stringify(pages),
+    });
+
+    return res.json(result);
   } catch (error) {
     next(error);
   }
